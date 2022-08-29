430560b64efa9d6ed98f7a9deae9bb0080e9545b
Weslley <48526020+wesnds@users.noreply.github.com>
2022-08-29 16:09:08 -0400

    uploading FCC approval

--- index.js
+++ index.js
@@ -3,31 +3,33 @@ const cors = require('cors');
 require('dotenv').config()
 const app = express();
 const bodyParser = require('body-parser');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use('/public', express.static(process.cwd() + '/public')); 
+
+const PORT = 3000;
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-const port = process.env.PORT || 3000;
-app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+app.listen(PORT || 3000, function () {
+  console.log('Node.js listening on port ' + PORT);
 });
 
-/*submit a form that includes a file upload. The form file input field has the name attribute set to upfile. When a file is submited, a response with the file name, type, and size in bytes returns in the JSON response.
+/*submit a form that has the name attribute set to upfile that includes a file upload. When a file is submited, a response with the file name, type, and size in bytes returns in the JSON response. 
 */
-app.post('/api/fileanalyse', (req, res) => {
-  const { upfile } = req.body;
-  const { size } = upfile;
-  const { type } = upfile;
-  const { name } = upfile;
-  res.json({
-    name,
-    type,
-    size
-  });
-});
+let responseObj ={}
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  responseObj = {
+    "name": req.file.originalname,
+    "type": req.file.mimetype,
+    "size": req.file.size
+  }
+  res.json(responseObj)
+  console.log(responseObj)
+})
